e47b36a0dab16755a3c08a319ec0c16335bc4db3
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2023-05-06 00:03:43 +0530

    Adjust border for card

--- components/base/Card.vue
+++ components/base/Card.vue
@@ -13,6 +13,8 @@
   width: 280px;
   background: var(--color-card-bg);
   border: 1px solid #cccccc20;
+  border-color: var(--color-card-border);
+  box-shadow: 0px 0px 2px 0px var(--color-slate-gray-opacity);
   border-radius: 1rem;
   transition: border-color 0.2s ease, box-shadow 0.2s ease;
 }
